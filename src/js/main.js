2f651f5a0c0878d8a66e7d2fca1fb8129e1f3f72
Joe Cloughley <joecloughley@gmail.com>
2016-12-29 13:23:17 +0700

    Modifications to base Sass file, base JS file. Remove unnecessary pages

--- src/js/main.js
+++ src/js/main.js
@@ -1,8 +1,7 @@
 $(document).ready(function() {
 
-	console.log('initializing js...'); 
-
 	//Initializing Mobile Menu
+	
 	$('#menu-toggle').click(function() {
 		$('body').toggleClass('mobile-menu-active');
 	});
@@ -22,19 +21,18 @@ $(document).ready(function() {
     	prevArrow: '<div class="prev-arrow"></div>',
     	nextArrow: '<div class="next-arrow"></div>',
     	responsive: [
-	    {
-	      breakpoint: 1024,
-	      settings: {
-	        arrows: true
-	      }
-	    }
-	  ]
+		    {
+		    	breakpoint: 1024,
+		      	settings: {
+		        	arrows: true
+		      	}
+		    }
+	  	]
 	});
 
 	$(window).scroll(function() {
     	var scrollPosition = $(document).scrollTop();
     	var footerHeight = $('.footer-text').height();
-    	    console.log('footer-height:' + footerHeight);
 
     	if (scrollPosition == 0 ) {
     		$('.page-scroller').addClass('holding');
@@ -43,7 +41,6 @@ $(document).ready(function() {
     	}
 
     if ($(window).scrollTop() == $(document).height()-$(window).height()){
-	    	console.log('bottom') ;
     		$('.page-scroller').css('bottom', footerHeight + 30 + 'px');
 		} else {
 			$('.page-scroller').css('bottom', '30px');
